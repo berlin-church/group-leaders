96c14621fe1761c48174a10e38f3c62fff4fe521
Giulliano Bueno <giulliano.bueno@blacklane.com>
2019-10-23 20:00:06 +0200

    Simple way to add attributes to a link

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 const Index = () => (
     <div>
-      <Link href="/about" title="About Page">
-        <a>About Page</a>
+      <Link href="/about">
+        <a title="About page">About Page</a>
       </Link>
       <p>Hello Next.js</p>
     </div>

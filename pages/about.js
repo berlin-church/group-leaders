c6ac5a22c78d649448461fc312c53ce5b0bb8a18
Giulliano Bueno <giulliano.bueno@blacklane.com>
2019-10-23 19:50:38 +0200

    Simple NextJS skeleton.

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const About = () => (
+    <div>
+    <Link href="/">
+      <a>Back</a>
+    </Link>
+      <p>This is the about page</p>
+    </div>
+  );
+  
+  export default About;
